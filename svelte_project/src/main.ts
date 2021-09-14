d3e6bed1e76eb182659db3e5fce37ef28b935075
dadiborn <36768799+dadiborn@users.noreply.github.com>
2021-09-14 17:19:16 +0300

    Update main.ts

--- svelte_project/src/main.ts
+++ svelte_project/src/main.ts
@@ -1,20 +1,16 @@
 import App from './App.svelte';
-// because we dig outside root folder:
-// 1. no import suggestions
-// 2. if you know what to import and write it down, declarations will be incorrect and taken from unknown places.
-// notes: this problems only present in Svelte component.
+// works but in very strange way 
 import {some_obj as objOutsideRootDir} from './../../other_project/lib'
-objOutsideRootDir; // << incorrect declaration + no suggestion for .a.b.c
+objOutsideRootDir.a.b;
 
 
 // if inside root folder everything works
-// import suggestion and declarations are correct
 import {some_obj as objInsideRootDir} from './../if_in_root/lib';
-objInsideRootDir.a; // << correct declaration
+objInsideRootDir.a.b;
 
 //using global.ts
-imFromGlobal_Outside.a; // << suggestion works but icon is (F), not line in .svelte component
-imFromGlobal_Inside.a; // << works and correct icon P
+imFromGlobal_Outside.a; // << suggestion works but icon is (F)
+imFromGlobal_Inside.a; // << works
 
 const app = new App({
 	target: document.body,
